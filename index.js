5b1cdce946f60863928540c102e2b4cd33303787
josiah <918780@dpsk12.net>
2022-04-30 10:00:19 -0600

    fixed first frame

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const fs = require('fs')
 var videoshow = require('videoshow')
 
 var maxIterations = 450, minX = -2, maxX = 2, 
-    minY = -2, maxY = 2, wid, hei,
-    jsX, jsY;
+	minY = -2, maxY = 2, wid, hei,
+	jsX, jsY;
 
-wid = 1000
-hei = 1000
+wid = 5000
+hei = 5000
 
 var lp = 0;
 var frames = 100
 
 let images = []
 
-jsX = Math.sin((2*Math.PI)/(lp/frames))
-jsY = Math.cos((2*Math.PI)/(lp/frames))
+jsX = Math.sin((2*Math.PI)*(lp/frames))
+jsY = Math.cos((2*Math.PI)*(lp/frames))
 
 const canvas = createCanvas(wid, hei)
 const ctx = canvas.getContext('2d')
@@ -32,66 +32,66 @@ ctx.fillStyle = 'white'
 ctx.fillRect(0,0,wid,hei)
  
 function remap( x, t1, t2, s1, s2 ) {
-    var f = ( x - t1 ) / ( t2 - t1 ),
-        g = f * ( s2 - s1 ) + s1;
-    return g;
+	var f = ( x - t1 ) / ( t2 - t1 ),
+		g = f * ( s2 - s1 ) + s1;
+	return g;
 }
 function getColor( c ) {
-    var r, g, b, p = c / 32,
-        l = ~~( p * 6 ), o = p * 6 - l, 
-        q = 1 - o;
+	var r, g, b, p = c / 32,
+		l = ~~( p * 6 ), o = p * 6 - l, 
+		q = 1 - o;
  
-    switch( l % 6 ) {
-        case 0: r = 1; g = o; b = 0; break;
-        case 1: r = q; g = 1; b = 0; break;
-        case 2: r = 0; g = 1; b = o; break;
-        case 3: r = 0; g = q; b = 1; break;
-        case 4: r = o; g = 0; b = 1; break;
-        case 5: r = 1; g = 0; b = q; break;
-    }
-    var c = "#" + ( "00" + ( ~~( r * 255 ) ).toString( 16 ) ).slice( -2 ) + 
-                  ( "00" + ( ~~( g * 255 ) ).toString( 16 ) ).slice( -2 ) + 
-                  ( "00" + ( ~~( b * 255 ) ).toString( 16 ) ).slice( -2 );
-    return (c);
+	switch( l % 6 ) {
+		case 0: r = 1; g = o; b = 0; break;
+		case 1: r = q; g = 1; b = 0; break;
+		case 2: r = 0; g = 1; b = o; break;
+		case 3: r = 0; g = q; b = 1; break;
+		case 4: r = o; g = 0; b = 1; break;
+		case 5: r = 1; g = 0; b = q; break;
+	}
+	var c = "#" + ( "00" + ( ~~( r * 255 ) ).toString( 16 ) ).slice( -2 ) + 
+				  ( "00" + ( ~~( g * 255 ) ).toString( 16 ) ).slice( -2 ) + 
+				  ( "00" + ( ~~( b * 255 ) ).toString( 16 ) ).slice( -2 );
+	return (c);
 }
 function render() {
-    var a, as, za, b, bs, zb, cnt, clr
-    for( var j = 0; j < hei; j++ ) {
-        for( var i = 0; i < wid; i++ ) {
-            a = remap( i, 0, wid, minX, maxX )
-            b = remap( j, 0, hei, minY, maxY )
-            cnt = 0;
-            while( ++cnt < maxIterations ) {
-                za = a * a; zb = b * b;
-                if( za + zb > 4 ) break;
-                as = za - zb; bs = 2 * a * b;
-                a = as + jsX; b = bs + jsY;
-            }
-            if( cnt < maxIterations ) {
-                ctx.fillStyle = getColor( cnt );
-            }
-            ctx.fillRect( i, j, 1, 1 );
-        }
-    }
-    save()
+	var a, as, za, b, bs, zb, cnt, clr
+	for( var j = 0; j < hei; j++ ) {
+		for( var i = 0; i < wid; i++ ) {
+			a = remap( i, 0, wid, minX, maxX )
+			b = remap( j, 0, hei, minY, maxY )
+			cnt = 0;
+			while( ++cnt < maxIterations ) {
+				za = a * a; zb = b * b;
+				if( za + zb > 4 ) break;
+				as = za - zb; bs = 2 * a * b;
+				a = as + jsX; b = bs + jsY;
+			}
+			if( cnt < maxIterations ) {
+				ctx.fillStyle = getColor( cnt );
+			}
+			ctx.fillRect( i, j, 1, 1 );
+		}
+	}
+	save()
 }
 
 function save(){
 
 	let base64Image = canvas.toDataURL().split(';base64,').pop();
 	fs.writeFile(`images/img${lp}.png`, base64Image, {encoding: 'base64'}, function(err) {
-	    console.log(`File ${lp+" out of "+frames} created`);
-        //images.push({path:`images/img${lp}.png` , loop: 0.5})
-        lp++
-        if(lp < frames){
-            jsX = Math.sin((2*Math.PI)*(lp/frames))
-            jsY = Math.cos((2*Math.PI)*(lp/frames))
+		console.log(`File ${lp+" out of "+frames} created`);
+		//images.push({path:`images/img${lp}.png` , loop: 0.5})
+		lp++
+		if(lp < frames){
+			jsX = Math.sin((2*Math.PI)*(lp/frames))
+			jsY = Math.cos((2*Math.PI)*(lp/frames))
 
-            console.log(lp/frames)
-            render()
-        }else{
-            //createVideo();
-        }
+			console.log(lp/frames)
+			render()
+		}else{
+			//createVideo();
+		}
 	});
 }
 
